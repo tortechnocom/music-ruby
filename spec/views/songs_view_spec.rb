733bc5f294f8fb3c4975c8efe80913914d104b76
Supachai Chaimangua (Tor) <tortechnocom@gmail.com>
2018-06-25 09:56:29 +0700

    Re format code

--- spec/views/songs_view_spec.rb
+++ spec/views/songs_view_spec.rb
@@ -4,15 +4,15 @@ describe 'song checking' , js: true do
   it 'check 3 songs in html' do
     visit '/song/html'
     # save_screenshot('/home/tor/one-dee/song-html.png', :full => true)
-    within('table') do
-      expect(page).to have_xpath("/html/body/table/tbody/tr", count: 3)
-    end
+    expect(page).to have_xpath("/html/body/table/tbody/tr", count: 3)
   end
+
   it 'check 3 songs in json' do
     visit 'song/json'
     # save_screenshot('/home/tor/one-dee/song-json.png', :full => true)
     expect(page).to have_text('"id":', count: 3)
   end
+  
   it 'check 3 columns in html' do
     visit 'song/html'
     within('table') do
