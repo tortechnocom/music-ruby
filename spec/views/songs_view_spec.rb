af025973707404f7ff0a1fda592d19905b972c9b
Supachai Chaimangua (Tor) <tortechnocom@gmail.com>
2018-06-26 09:53:35 +0700

    Check content in table

--- spec/views/songs_view_spec.rb
+++ spec/views/songs_view_spec.rb
@@ -15,10 +15,18 @@ describe 'song checking' , js: true do
   
   it 'check 3 columns in html' do
     visit 'song/html'
+    # check headers
     within('table') do
       expect(page).to have_xpath("thead/tr/th[1]", text: "Name")
       expect(page).to have_xpath("thead/tr/th[2]", text: "Brand")
       expect(page).to have_xpath("thead/tr/th[3]", text: "Album")
+    end
+    # check content
+    3.times do |index1|
+      3.times do |index2|
+        content = find(:xpath, "/html/body/table/tbody/tr[#{index1+1}]/td[#{index2+1}]").text
+        expect(content).not_to eq('') 
+      end
     end
   end
 end
